cb0615cbe69353b7e5f5c9e00c16e7313e61c389
Antonio Matos <antoniomvmatos@gmail.com>
2026-01-11 21:58:58 +0000

    PR/DeleteMethod

--- Savings.Api/Savings.Api/Controllers/ValuesController.cs
+++ Savings.Api/Savings.Api/Controllers/ValuesController.cs
@@ -54,7 +54,7 @@ namespace Savings.Api.Controllers
             return Ok(snapshot);
         }
 
-        // DELETE /api/snapshots/{id}  (opcional, mas útil)
+        // DELETE /api/snapshots/{id}  (opcional, mas útil teste deploy)
         [HttpDelete("{id}")]
         public IActionResult Delete(int id)
         {
