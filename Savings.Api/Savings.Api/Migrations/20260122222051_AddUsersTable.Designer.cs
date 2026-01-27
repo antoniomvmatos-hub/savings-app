0ed9942b35237bad40580cdc23edaa97b29644ad
Antonio Matos <antoniomvmatos@gmail.com>
2026-01-27 12:03:21 +0000

    Authentication backend

--- Savings.Api/Savings.Api/Migrations/20260122222051_AddUsersTable.Designer.cs
+++ Savings.Api/Savings.Api/Migrations/20260122222051_AddUsersTable.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Savings.Api.Data;
 
@@ -10,9 +11,11 @@ using Savings.Api.Data;
 namespace Savings.Api.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260122222051_AddUsersTable")]
+    partial class AddUsersTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
